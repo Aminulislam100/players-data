7b62c69b9ba5f40f49ac461338c2fc3f38f7e753
Aminulislam100 <137256592+Aminulislam100@users.noreply.github.com>
2025-04-07 15:14:18 +0600

    Create players.jsx

---
+++ players.jsx
@@ -0,0 +1,112 @@
+[
+  {
+    "name": "Lionel Messi",
+    "position": "Forward",
+    "club": "Inter Miami",
+    "nationality": "Argentina",
+    "age": 37,
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/8/89/Lionel_Messi_20180626.jpg",
+    "ballonDOr": 8,
+    "worldCups": 1,
+    "allTrophies": 44
+  },
+  {
+    "name": "Cristiano Ronaldo",
+    "position": "Forward",
+    "club": "Al Nassr",
+    "nationality": "Portugal",
+    "age": 40,
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg",
+    "ballonDOr": 5,
+    "worldCups": 0,
+    "allTrophies": 35
+  },
+  {
+    "name": "Kylian Mbappé",
+    "position": "Forward",
+    "club": "Paris Saint-Germain",
+    "nationality": "France",
+    "age": 26,
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/a/a8/Kylian_Mbappé_2019.jpg",
+    "ballonDOr": 0,
+    "worldCups": 1,
+    "allTrophies": 17
+  },
+  {
+    "name": "Erling Haaland",
+    "position": "Forward",
+    "club": "Manchester City",
+    "nationality": "Norway",
+    "age": 24,
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/6/6e/Erling_Haaland_2023.jpg",
+    "ballonDOr": 0,
+    "worldCups": 0,
+    "allTrophies": 8
+  },
+  {
+    "name": "Kevin De Bruyne",
+    "position": "Midfielder",
+    "club": "Manchester City",
+    "nationality": "Belgium",
+    "age": 33,
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/f/f6/Kevin_De_Bruyne_2021.jpg",
+    "ballonDOr": 0,
+    "worldCups": 0,
+    "allTrophies": 20
+  },
+  {
+    "name": "Jude Bellingham",
+    "position": "Midfielder",
+    "club": "Real Madrid",
+    "nationality": "England",
+    "age": 21,
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/7/7c/Jude_Bellingham_2020.jpg",
+    "ballonDOr": 0,
+    "worldCups": 0,
+    "allTrophies": 3
+  },
+  {
+    "name": "Virgil van Dijk",
+    "position": "Defender",
+    "club": "Liverpool",
+    "nationality": "Netherlands",
+    "age": 33,
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/5/5e/Virgil_van_Dijk_2018.jpg",
+    "ballonDOr": 0,
+    "worldCups": 0,
+    "allTrophies": 7
+  },
+  {
+    "name": "Alisson Becker",
+    "position": "Goalkeeper",
+    "club": "Liverpool",
+    "nationality": "Brazil",
+    "age": 32,
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/3/3e/Alisson_becker.jpg",
+    "ballonDOr": 0,
+    "worldCups": 0,
+    "allTrophies": 9
+  },
+  {
+    "name": "Pedri",
+    "position": "Midfielder",
+    "club": "Barcelona",
+    "nationality": "Spain",
+    "age": 22,
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/f/f3/Pedri_2021.jpg",
+    "ballonDOr": 0,
+    "worldCups": 0,
+    "allTrophies": 4
+  },
+  {
+    "name": "Achraf Hakimi",
+    "position": "Defender",
+    "club": "Paris Saint-Germain",
+    "nationality": "Morocco",
+    "age": 26,
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/5/59/Achraf_Hakimi_2022.jpg",
+    "ballonDOr": 0,
+    "worldCups": 0,
+    "allTrophies": 12
+  }
+]
